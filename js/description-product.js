744ef7765838e0aafe49c14206e5380c8208adae
Mario Oliveira <mario.oliveirag12@gmail>
2018-12-18 23:02:51 +0000

    [013] - Desafio Avançado

--- js/description-product.js
+++ js/description-product.js
@@ -7,13 +7,15 @@ function hardleClick() {
 }
 
 
-/* Exercicio Basico */
+/* Exercicios */
 
 const $button = window.document.querySelector(".button-store.-second");
+const $textoCarrinho = window.document.querySelector(".header-store .action.-last");
 
 $button.addEventListener("click", buttonClick)
 
 function buttonClick() {
     console.log("clicou no botão");
     alert("Adicionou");
+    $textoCarrinho.innerHTML = "Carrinho (11)";
 }

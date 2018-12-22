efc285ec4aca69fc25a5cbf26f967257273f825f
Mario Oliveira <mario.oliveirag12@gmail>
2018-12-22 18:52:49 +0000

    [015] - Desafio Único

--- js/description-product.js
+++ js/description-product.js
@@ -1,16 +1,34 @@
-const $heart = window.document.querySelector(".-heart");
+const $heart = window.document.querySelector(".-heart"); // selecioa um elemnetos 
+const $stars = window.document.querySelectorAll(".cont-star .star"); // seleciona todos os elementos All
 
 $heart.addEventListener("click", hardleClick)
 
+//  função da estrela
+// percorre a lista usando o forEach, retornando cada elemento
+
+
+$stars.forEach(($star, index_selected) => {
+    $star.addEventListener("click", function() {
+        $stars.forEach((star, index) => {
+            if (index_selected >= index) {
+                star.classList.add("-active");
+            } else {
+                star.classList.remove("-active");
+            }
+        });
+    });
+});
+
+
+
+
+
+
+
+
+
+
+
 function hardleClick() {
-    /* if ($heart.classList.contains("-active")) {
-         $heart.classList.add("-active");
-         console.log('ativo');
-     } else {
-         $heart.classList.remove("-active");
-         console.log('removel');
-     }*/
-
-    $heart.classList.toggle("-active"); // mesma coisa que o IF/Else
-
+    this.classList.toggle("-active");
 }
